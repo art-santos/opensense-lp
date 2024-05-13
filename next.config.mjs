8296247c6e0a29ba1e4b3da2a2c075c62fd129fa
Arthur Santos <arthursantosdeveloper@gmail.com>
2024-05-13 02:21:24 -0300

    chore: new structure for lp

--- next.config.mjs
+++ next.config.mjs
@@ -14,6 +14,20 @@ import { withContentlayer } from 'next-contentlayer'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  
   images: {
     domains: ['images.unsplash.com','i.ytimg.com','ytimg.com'],
 },
